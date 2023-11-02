04c0f14d9569324d3f24680fb064ead2ffd1c999
altpass <altpass@yandex.ru>
2023-11-02 16:21:14 +0300

    PR

--- ewm/src/main/java/ru/practicum/repository/IpRepository.java
+++ ewm/src/main/java/ru/practicum/repository/IpRepository.java
@@ -1,14 +1,17 @@
 package ru.practicum.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.model.UniIp;
 
 @EnableJpaRepositories
 @Transactional
 public interface IpRepository extends Repository<UniIp, Long> {
-    boolean existsByIpAddressAndEventId(String ipAddress, Long eventId);
+    @Query(value = "SELECT case when count(u)> 0 then true else false end FROM UniIp u WHERE u.ipAddress = :ipAddress AND u.event.id like :eventId")
+    boolean existsByIpAddressAndEventId(@Param("ipAddress") String ipAddress, @Param("eventId") Long eventId);
 
     void save(UniIp uniIp);
 }
