98190e295d09f1fb61cb457719ce75cf1132dfea
altpass <altpass@yandex.ru>
2023-11-02 18:10:38 +0300

    PR

--- stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -15,8 +15,8 @@ import java.util.List;
 public interface  StatsRepository extends Repository<Hit, Long> {
     @Query(value = "SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(h.ip) AS hits) " +
             "FROM Hit h " +
-            "WHERE h.timestamp " +
-            "BETWEEN :start AND :end " +
+            "WHERE cast(h.timestamp as date) " +
+            "BETWEEN cast(:start as date) AND cast(:end as date) " +
             "AND h.uri = :uri " +
             "GROUP BY h.uri, h.app " +
             "ORDER BY hits ASC")
@@ -24,7 +24,7 @@ public interface  StatsRepository extends Repository<Hit, Long> {
 
     @Query(value = "SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip) AS hits) " +
             "FROM Hit h " +
-            "WHERE h.timestamp BETWEEN :start AND :end " +
+            "WHERE cast(h.timestamp as date) BETWEEN cast(:start as date) AND cast(:end as date) " +
             "AND h.uri = :uri " +
             "GROUP BY h.uri, h.app " +
             "ORDER BY hits ASC")
@@ -32,16 +32,16 @@ public interface  StatsRepository extends Repository<Hit, Long> {
 
     @Query(value = "SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip) AS hits) " +
             "FROM Hit h " +
-            "WHERE h.timestamp " +
-            "BETWEEN :start AND :end " +
+            "WHERE cast(h.timestamp as date) " +
+            "BETWEEN cast(:start as date) AND cast(:end as date) " +
             "GROUP BY h.uri, h.app " +
             "ORDER BY hits DESC")
     List<ViewStatsDto> findAllEmptyUrisUnique(LocalDateTime start, LocalDateTime end);
 
     @Query(value = "SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(h.ip) AS hits) " +
             "FROM Hit h " +
-            "WHERE h.timestamp " +
-            "BETWEEN :start AND :end " +
+            "WHERE cast(h.timestamp as date) " +
+            "BETWEEN cast(:start as date) AND cast(:end as date) " +
             "GROUP BY h.uri, h.app " +
             "ORDER BY hits DESC")
     List<ViewStatsDto> findAllEmptyUrisNotUnique(LocalDateTime start, LocalDateTime end);
